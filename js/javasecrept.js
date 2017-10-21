9e61194ea2ff1894caaf3bbdc2fe3f5bd2ec7a85
hassanmahdihussien <hassanmahdic@gmail.com>
2017-10-21 09:17:58 +0300

    calculater

---
+++ js/javasecrept.js
@@ -0,0 +1,14 @@
+function dis(x){
+
+    frm.txt.value=frm.txt.value+x;
+}
+
+function ce(){
+    frm.txt.value="";
+
+}
+
+function bs(){
+
+    frm.txt.value=frm.txt.value.slice(0,frm.txt.value.length-1);
+}
